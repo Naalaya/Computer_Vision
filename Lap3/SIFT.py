374ec3f48c7c4be716c4c410b8d4a98ecff4a0c2
Naalaya <anpt203@gmail.com>
2024-04-12 06:53:31 +0700

    SIFT

--- Lap3/SIFT.py
+++ Lap3/SIFT.py
@@ -1,33 +1,24 @@
 import cv2
-import numpy as np
+import matplotlib.pyplot as plt
 
-# Load images
-img1 = cv2.imread('image1.jpg', cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread('image2.jpg', cv2.IMREAD_GRAYSCALE)
+# read images
+img1 = cv2.imread('eiffel_2.jpeg')
+img2 = cv2.imread('eiffel_1.jpg')
 
-# Initialize SIFT detector
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+#sift
 sift = cv2.SIFT.create()
 
-# Detect keypoints and compute descriptors
-keypoints1, descriptors1 = sift.detectAndCompute(img1, None)
-keypoints2, descriptors2 = sift.detectAndCompute(img2, None)
+keypoints_1, descriptors_1 = sift.detectAndCompute(img1,None)
+keypoints_2, descriptors_2 = sift.detectAndCompute(img2,None)
 
-# Initialize brute force matcher
-bf = cv2.BFMatcher()
+#feature matching
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 
-# Match descriptors
-matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+matches = bf.match(descriptors_1,descriptors_2)
+matches = sorted(matches, key = lambda x:x.distance)
 
-# Apply ratio test
-good_matches = []
-for m, n in matches:
-    if m.distance < 0.75 * n.distance:
-        good_matches.append(m)
-
-# Draw matches
-img_matches = cv2.drawMatches(img1, keypoints1, img2, keypoints2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
-# Display the result
-cv2.imshow('Matches', img_matches)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:50], img2, flags=2)
+plt.imshow(img3),plt.show()
